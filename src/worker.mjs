35b307900a46c705dfb356ed745f2c25b684dfa3
johnd0e <1838643+johnd0e@users.noreply.github.com>
2024-07-25 23:21:53 +0200

    Update api client version

--- src/worker.mjs
+++ src/worker.mjs
@@ -42,7 +42,7 @@ const handleOPTIONS = async () => {
 const BASE_URL = "https://generativelanguage.googleapis.com";
 const API_VERSION = "v1beta";
 // https://github.com/google/generative-ai-js/blob/0931d2ce051215db72785d76fe3ae4e0bc3b5475/packages/main/src/requests/request.ts#L67
-const API_CLIENT = "genai-js/0.5.0"; // npm view @google/generative-ai version
+const API_CLIENT = "genai-js/0.16.0"; // npm view @google/generative-ai version
 async function handleRequest(req, apiKey) {
   const MODEL = "gemini-1.5-pro-latest";
   const TASK = req.stream ? "streamGenerateContent" : "generateContent";
